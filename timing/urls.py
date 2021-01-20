987242ccbef75799f2bc9ae73c79c291b9081980
Steveen <steveen.echeverri@gmail.com>
2021-01-20 12:19:34 -0500

    Create page template edit profile

--- timing/urls.py
+++ timing/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from apps.core.views import frontpage, privacy, plans
-from apps.userprofile.views import signup, myaccount
+from apps.userprofile.views import signup, myaccount, edit_profile
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
@@ -30,4 +30,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('myaccount/', myaccount, name='myaccount'),
+    path('myaccount/edit_profile', edit_profile, name='edit_profile'),
 ]
