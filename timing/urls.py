e069eaaad99a37ef8d8808b3ac5fed9c2c6f0fb5
Steveen <steveen.echeverri@gmail.com>
2021-01-23 22:52:30 -0500

    New basic templated team

--- timing/urls.py
+++ timing/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('myaccount/', include('apps.userprofile.urls')),
+    path('myaccount/teams/', include('apps.team.urls')),
 ]
