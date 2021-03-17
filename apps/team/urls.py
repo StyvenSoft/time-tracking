e9dce07176e0da5ce56f101005b42d2261948e75
Steveen <steveen.echeverri@gmail.com>
2021-03-16 23:02:55 -0500

    Cretate Team for button details

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add
+from .views import team, add
 
 app_name = 'team'
 
 urlpatterns = [
     path('add/', add, name='add'),
+    path('<int:team_id>/', team, name='team')
 ]
