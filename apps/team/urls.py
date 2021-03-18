2e9f5845c600c1c076613a7d29be4da18aef40f3
Steveen <steveen.echeverri@gmail.com>
2021-03-17 23:08:01 -0500

    Created New template for edit team

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import team, add
+from .views import team, add, edit
 
 app_name = 'team'
 
 urlpatterns = [
     path('add/', add, name='add'),
+    path('edit/', edit, name='edit'),
     path('<int:team_id>/', team, name='team')
 ]
--- apps/team/views.py
+++ apps/team/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Team
+from django.contrib import messages
 
 # Views
 
@@ -27,3 +28,20 @@ def add(request):
             return redirect('myaccount')
     
     return render(request, 'team/add.html')
+
+@login_required
+def edit(request):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE, members__in=[request.user])
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+        if title:
+            team.title = title
+            team.save()
+
+            message.info(request, 'The changes was saved')
+
+            return redirect('team:team', team_id=team.id)
+    
+    return render(request, 'team/edit.html', {'team': team})
