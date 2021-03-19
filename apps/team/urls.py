2e68ce1dca2e38db93dcfab8c5cbee52bf11e9b0
Steveen <steveen.echeverri@gmail.com>
2021-03-18 22:35:06 -0500

    Team activate in my account userprofile

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import team, add, edit
+from .views import team, add, edit, activate_team
 
 app_name = 'team'
 
 urlpatterns = [
     path('add/', add, name='add'),
     path('edit/', edit, name='edit'),
+    path('activate_team/<int:team_id>/', activate_team, name='activate_team'),
     path('<int:team_id>/', team, name='team')
 ]
